6f7f695a8597f3eb2196a90d741a98377035b0d6
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-12-10 18:08:17 -0800

    Update Salesmans-travel.js

--- Salesmans-travel.js
+++ Salesmans-travel.js
@@ -25,3 +25,22 @@ You can see a few addresses and zipcodes in the test cases.
 
 
 // Solution
+
+function travel(r, zipcode) {
+  const addressList = r.split(",");
+
+  const streets = [];
+  const numbers = [];
+
+  addressList.forEach(address => {
+      let zip = address.substring(address.length - 8, address.length);
+      if (zip === zipcode) {
+          let number = address.substring(0, address.indexOf(" ") + 1);
+          let street = address.substring(address.indexOf(" "), address.length -8);
+          
+          streets.push(street.trim());
+          numbers.push(number.trim());
+      }
+  });
+  return `${zipcode}:${streets.join(",")}/${numbers.join(",")}`;
+}
